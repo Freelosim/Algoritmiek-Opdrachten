4d7e71e601436329f21a0bad04b776dd07a6d40d
Casper van den Dijk <486843@student.fontys.nl>
2022-04-12 17:48:32 +0200

    Add project files.

---
+++ Opdracht 2/Opdracht 2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Opdracht_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int fibboVal = 0;
+            int prevValue = 0;
+            int sum = 0;
+
+            while (fibboVal <= 4000000)
+            {
+                if (fibboVal % 2 == 0)
+                {
+                    sum += fibboVal;
+                }
+
+                int tempPrevValue = fibboVal;
+
+                if (fibboVal <= 1)
+                {
+                    fibboVal++;
+                }
+                else
+                {
+                    fibboVal += prevValue;
+                }
+
+                prevValue = tempPrevValue;
+
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ Opdracht 4/Opdracht 4.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Opdracht_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int palindrome = 0;
+            int firstNumber = 100;
+            int secondNumber = 100;
+
+            while (firstNumber < 1000)
+            {
+
+                while (secondNumber < 1000)
+                {
+                    int tempPalindrome = firstNumber * secondNumber;
+                    //Console.WriteLine($"{firstNumber} * {secondNumber} = {tempPalindrome}");
+                    string _palindrome = tempPalindrome.ToString();
+
+
+                    for (int i = 0; i < _palindrome.Length; i++)
+                    {
+                        if (_palindrome[i] == _palindrome[_palindrome.Length - 1 - i])
+                        {
+                            if (i == _palindrome.Length - i)
+                            {
+                                if (tempPalindrome > palindrome)
+                                {
+                                    palindrome = tempPalindrome;
+                                }
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            break;
+                        }
+
+                    }
+
+
+                    secondNumber++;
+                }
+                secondNumber = 100;
+                firstNumber++;
+            }
+            Console.WriteLine($"Palindrome: {palindrome}");
+        }
+    }
+}
