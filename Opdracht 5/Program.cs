2b18e2511cbf55b248a389a61d4840c23eaf1eef
Casper van den Dijk <486843@student.fontys.nl>
2022-05-13 14:25:07 +0200

    Opdracht 5 & 6 toegevoegd

---
+++ Opdracht 5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Opdracht_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int answer = 0;
+            bool answerBool = false;
+
+            while (!answerBool)
+            {
+                answer+=20;
+                bool divide = false;
+
+                for (int i = 19; i > 0; i--)
+                {
+                    //Console.WriteLine($"{answer} % {i}");
+                    if (answer % i != 0)
+                    {
+                        divide = true;
+                        break;
+                    }
+                }
+                answerBool = !divide;
+            }
+            Console.WriteLine($"Answer = {answer}");
+        }
+    }
+}
---
+++ Opdracht 6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Opdracht_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int y = 0;
+            int x = 0;
+
+            for (int i = 1; i < 101; i++)
+            {
+                y+=(ii);
+                x = x + i;
+            }
+            x=x;
+            Console.WriteLine($"{x - y}");
+        }
+    }
+}
